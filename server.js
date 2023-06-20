08c695df8a1f6bce6caca7cd54cbd947e46dc5c4
Aditya Prajapati <63953219+Aditya-Prajapati@users.noreply.github.com>
2023-06-21 00:03:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const tweetRoute = require("./routes/tweet");
 const app = express();
 
 app.use(cors(
-    origin: "https://twitter-clone-frontend-in-progress.vercel.app/",
+    origin: "https://twitter-clone-frontend-in-progress.vercel.app",
     credentials: true
 ));
 app.use(bodyParser.json());
