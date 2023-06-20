830db96bd34f8bae3fb65da24d61b443095af4a6
Aditya Prajapati <63953219+Aditya-Prajapati@users.noreply.github.com>
2023-06-20 22:28:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(User.createStrategy());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://twitter-clone-frontend-in-progress.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use("/auth", authRoute);
 app.use("/tweet", tweetRoute);
 
