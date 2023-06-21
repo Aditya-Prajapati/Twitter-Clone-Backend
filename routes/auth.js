1bd15986dac3c94ded4abcddce7e92126efcd740
Aditya Prajapati <63953219+Aditya-Prajapati@users.noreply.github.com>
2023-06-21 23:43:52 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -142,12 +142,13 @@ app.get("/login/failure", (req, res) => {
     })
 })
 
-app.get("/logout", (req, res) => {
-
-    req.logout();
-    req.session.destroy();
-    res.clearCookie("connect.sid"); 
-    res.redirect("https://twitter-clone-frontend-in-progress.vercel.app/");
+app.post("/logout", function(req, res, next){
+    req.logout(function(err) {
+      if (err) { return next(err); }
+        req.session.destroy();
+        res.clearCookie("connect.sid"); 
+        res.redirect('/');
+    });
 })
 
 module.exports = app;
